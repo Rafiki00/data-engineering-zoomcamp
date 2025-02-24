238b6f6740e70a3285bc72d07b505d3d14e2b35b
Rafael Llopis <rafa94el@gmail.com>
2025-02-24 22:42:10 +0000

    h

--- 04-analytics-engineering/taxi_rides_ny/models/core/dim_date.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/dim_date.sql
@@ -1,29 +1,14 @@
-{{ config(materialized='table') }}
-
 WITH date_series AS (
-    -- Generate a list of dates from the earliest to the latest pickup date
     SELECT 
-        DATE_ADD(
-            (SELECT MIN(DATE(pickup_datetime)) FROM {{ ref('fact_trips') }}), 
-            INTERVAL n DAY
-        ) AS date_actual
-    FROM UNNEST(GENERATE_ARRAY(0, 
-        DATE_DIFF(
-            (SELECT MAX(DATE(pickup_datetime)) FROM {{ ref('fact_trips') }}), 
-            (SELECT MIN(DATE(pickup_datetime)) FROM {{ ref('fact_trips') }}),
-            DAY
-        )
-    )) AS n
+        DATE('2019-01-01') + INTERVAL n DAY AS date_day
+    FROM UNNEST(GENERATE_ARRAY(0, 365 * 10)) AS n -- 10 years of dates
 )
 
 SELECT 
-    date_actual,
-    EXTRACT(YEAR FROM date_actual) AS year,
-    EXTRACT(QUARTER FROM date_actual) AS quarter,
-    EXTRACT(MONTH FROM date_actual) AS month,
-    CONCAT(EXTRACT(YEAR FROM date_actual), '/Q', EXTRACT(QUARTER FROM date_actual)) AS year_quarter,
-    CONCAT(EXTRACT(YEAR FROM date_actual), '-', LPAD(EXTRACT(MONTH FROM date_actual), 2, '0')) AS year_month,
-    EXTRACT(WEEK FROM date_actual) AS week_of_year,
-    EXTRACT(DAY FROM date_actual) AS day_of_month,
-    EXTRACT(DAYOFWEEK FROM date_actual) AS day_of_week
-FROM date_series
+    date_day,
+    EXTRACT(YEAR FROM date_day) AS year,
+    EXTRACT(QUARTER FROM date_day) AS quarter,
+    EXTRACT(MONTH FROM date_day) AS month,
+    FORMAT_DATE('%Y/Q%Q', date_day) AS year_quarter,
+    FORMAT_DATE('%Y-%m', date_day) AS year_month
+FROM date_series;
